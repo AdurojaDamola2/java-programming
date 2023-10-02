5883e6ce1305a037c2f061a69c42b5904890c13f
Aduroja Damola <adurojadamola2@gmail.com>
2023-10-02 17:00:43 +0100

    cleaned whitespace

--- src/Tasks.java
+++ src/Tasks.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class Tasks {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
+
         int c;
         int thisIsAVariable;
         int q76354;
@@ -16,7 +18,5 @@ public class Tasks {
         if (number != 7 ){
             System.out.println("the variable number is not equal to 7 ");
         }
-
-
     }
 }
