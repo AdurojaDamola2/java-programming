0c7528855e0b52bdc3ac315f70177e9f4475a8f6
Aduroja Damola <adurojadamola2@gmail.com>
2023-10-02 17:01:58 +0100

    cleaned whitespace

--- src/Comparison.java
+++ src/Comparison.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 
 public class Comparison {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
         int number1;
         int number2;
-        System.out.println("Enter first interger");
+
+        System.out.println("Enter first integer");
         number1 = input.nextInt();
 
-        System.out.println("Enter second interger");
+        System.out.println("Enter second integer");
         number2 = input.nextInt();
 
         if (number1 == number2){
@@ -22,6 +24,5 @@ public class Comparison {
         if (number1 > number2){
             System.out.println("number1 is greater than number2 ");
         }
-
     }
 }
